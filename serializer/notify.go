6dee5666bdc0ab37c8fc4405662ba5921e7e580d
pascalpandey <pascalis.pandey@gmail.com>
2025-03-07 21:22:08 +0800

    add monitor

--- serializer/notify.go
+++ serializer/notify.go
@@ -7,17 +7,17 @@ import (
 )
 
 func NotifyBook(confirmationId string, startTime state.BookingTime, endTime state.BookingTime) string {
-	return fmt.Sprintf("NEW,%s,%s,%s", confirmationId, formatBookingTime(startTime), formatBookingTime(endTime))
+	return fmt.Sprintf("MONITOR,NEW,%s,%s,%s", confirmationId, formatBookingTime(startTime), formatBookingTime(endTime))
 }
 
 func NotifyOffset(confirmationId string, offsetTime state.BookingTime) string {
-	return fmt.Sprintf("OFFSET,%s,%s", confirmationId, formatBookingTime(offsetTime))
+	return fmt.Sprintf("MONITOR,OFFSET,%s,%s", confirmationId, formatBookingTime(offsetTime))
 }
 
 func NotifyExtend(confirmationId string, extendTime state.BookingTime) string {
-	return fmt.Sprintf("EXTEND,%s,%s", confirmationId, formatBookingTime(extendTime))
+	return fmt.Sprintf("MONITOR,EXTEND,%s,%s", confirmationId, formatBookingTime(extendTime))
 }
 
 func NotifyCancel(confirmationId string) string {
-	return fmt.Sprintf("CANCEL,%s", confirmationId)
+	return fmt.Sprintf("MONITOR,CANCEL,%s", confirmationId)
 }
